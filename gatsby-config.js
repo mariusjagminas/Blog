9c0a324162299661cd048bc24f67c74fbc8a3089
mariusjagminas <mariusjagminas.it@gmail.com>
2019-06-12 12:08:11 +0200

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
+  ],
+}
