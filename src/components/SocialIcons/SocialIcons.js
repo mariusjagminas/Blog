97ff83917861bb5a84a27d7d60463aa264bf401c
mariusjagminas <mariusjagminas.it@gmail.com>
2019-06-15 20:40:07 +0200

    Add SocialIcons component

--- src/components/SocialIcons/SocialIcons.js
+++ src/components/SocialIcons/SocialIcons.js
@@ -1,11 +1,41 @@
 import React from "react"
 import styled from "styled-components"
+import { FaFacebookF, FaTwitter } from "react-icons/fa"
 
 const IconsWrapper = styled.div`
-  
-  display: none;
+  display: flex;
+  justify-content: space-between;
 `
 
-const SocialIcons = () => <IconsWrapper>Hey</IconsWrapper>
+const StyledLink = styled.a`
+  display: none;
+  color: ${({ theme }) => theme.primary};
+  font-size: 16px;
+  margin-left: 20px;
+  transition: color 0.25s ease-in-out;
+
+  :hover {
+    color: ${({ theme }) => theme.secondary};
+  }
+
+  ${({ theme }) => theme.mq.laptop} {
+    display: initial;
+  }
+`
+
+const SocialIcons = () => (
+  <IconsWrapper>
+    <StyledLink
+      href={
+        "https://www.facebook.com/Litt%C3%A9rature-extr%C3%AAme-contemporaineLiteratura-najnowsza-180172648750803/"
+      }
+    >
+      <FaFacebookF />
+    </StyledLink>
+    <StyledLink href={"https://twitter.com/pawelhladki"}>
+      <FaTwitter />
+    </StyledLink>
+  </IconsWrapper>
+)
 
 export default SocialIcons
