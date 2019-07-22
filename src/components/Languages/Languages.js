bf09783a149f9c2dfcc5f10fdcca738ae71af3ea
mariusjagminas <mariusjagminas.it@gmail.com>
2019-07-22 17:56:02 +0200

    Change to flags language switcher

--- src/components/Languages/Languages.js
+++ src/components/Languages/Languages.js
@@ -1,41 +1,45 @@
 import React from 'react';
 import { IntlContextConsumer, changeLocale } from 'gatsby-plugin-intl';
 import styled from 'styled-components';
-
-const languageName = {
-	pl: 'Polish',
-	fr: 'Fraicais'
-};
+import flagPoland from '../../assets/images/flag-poland.png';
+import flagFrance from '../../assets/images/flag-france.png';
 
 const Wrapper = styled.div`
-	display: ${({ inMobileMenu }) => (inMobileMenu ? 'flex' : 'none')};
+	display: ${({ inMobileMenu }) => (inMobileMenu ? 'inline-flex' : 'none')};
 	${({ theme }) => theme.mq.laptop} {
-		display: ${({ inMobileMenu }) => (inMobileMenu ? 'none' : 'flex')};
+		display: ${({ inMobileMenu }) => (inMobileMenu ? 'none' : 'inline-flex')};
 	}
 `;
 
-const Language = props => {
+const ImgWrapper = styled.div`
+	width: 35px;
+	margin: 0 20px;
+
+	${({ theme }) => theme.mq.laptop} {
+		width: 25px;
+		margin: 0 10px;
+	}
+`;
+
+const StyledImg = styled.img`
+	width: 100%;
+	height: auto;
+`;
+
+const Language = ({ inMobileMenu }) => {
 	return (
-		<Wrapper {...props}>
-			<IntlContextConsumer>
-				{({ languages, language: currentLocale }) =>
-					languages.map(language => (
-						<div
-							key={language}
-							onClick={() => changeLocale(language)}
-							style={{
-								color: currentLocale === language ? `red` : `default`,
-								margin: 10,
-								textDecoration: `none`,
-								cursor: `pointer`
-							}}
-						>
-							{languageName[language]}
-						</div>
-					))
-				}
-			</IntlContextConsumer>
-		</Wrapper>
+		<IntlContextConsumer>
+			{() => (
+				<Wrapper inMobileMenu={inMobileMenu}>
+					<ImgWrapper onClick={() => changeLocale('pl')}>
+						<StyledImg src={flagPoland} />
+					</ImgWrapper>
+					<ImgWrapper onClick={() => changeLocale('fr')}>
+						<StyledImg src={flagFrance} />
+					</ImgWrapper>
+				</Wrapper>
+			)}
+		</IntlContextConsumer>
 	);
 };
 
