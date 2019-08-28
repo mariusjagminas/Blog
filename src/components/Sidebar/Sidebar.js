852d4bba2179f5f247dfa3efd29c2e9865191366
mariusjagminas <mariusjagminas.it@gmail.com>
2019-08-28 21:10:47 +0300

    Add HistoryOfTheaterList component

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,22 +1,24 @@
-import React from "react"
-import styled from "styled-components"
-import LatestArticles from "../LatestArticles/LatestArticles"
-import Archive from "../Archive/Archive"
+import React from 'react';
+import styled from 'styled-components';
+import LatestArticles from '../LatestArticles/LatestArticles';
+import Archive from '../Archive/Archive';
+import HistoryOfTheaterList from '../HistoryOfTheaterList/HistoryOfTheaterList';
 
 const StyledSidebar = styled.aside`
-  width: 320px;
-  margin-left: 14px;
-  padding: 40px 20px;
-  display: none;
-  ${({ theme }) => theme.mq.laptop} {
-    display: initial;
-  }
-`
+	width: 320px;
+	margin-left: 14px;
+	padding: 40px 20px;
+	display: none;
+	${({ theme }) => theme.mq.laptop} {
+		display: initial;
+	}
+`;
 
 const Sidebar = () => (
-  <StyledSidebar>
-    <LatestArticles />
-    <Archive />
-  </StyledSidebar>
-)
-export default Sidebar
+	<StyledSidebar>
+		<LatestArticles />
+		<HistoryOfTheaterList />
+		<Archive />
+	</StyledSidebar>
+);
+export default Sidebar;
