220cfeaf70f030a5cd6905f181134a5009852653
mariusjagminas <mariusjagminas.it@gmail.com>
2019-08-30 23:56:36 +0300

    Update rich text images css for mobile view

--- src/components/RichTextContenful/RichTextContenful.js
+++ src/components/RichTextContenful/RichTextContenful.js
@@ -1,52 +1,50 @@
-import React from "react"
-import { BLOCKS, MARKS, INLINES } from "@contentful/rich-text-types"
-import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
-import ContentfulImage from "../../assets/helpers/ContentfulImage"
-import styled from "styled-components"
+import React from 'react';
+import { BLOCKS, MARKS, INLINES } from '@contentful/rich-text-types';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import ContentfulImage from '../../assets/helpers/ContentfulImage';
+import styled from 'styled-components';
 
 const ImageWrapper = styled.div`
-  width: 250px;
-  float: right;
-  margin-left: 20px;
+	width: 100%;
 
-  &:nth-of-type(even) {
-    float: left;
-    margin-right: 20px;
-  }
-`
+	${({ theme }) => theme.mq.tablet} {
+		margin-left: 20px;
+		float: right;
+		width: 300px;
+		&:nth-of-type(even) {
+			float: left;
+			margin-right: 20px;
+		}
+	}
+`;
+
 const StyledB = styled.b`
-  color: ${({ theme }) => theme.primary};
-`
+	color: ${({ theme }) => theme.primary};
+`;
 
 const StyledA = styled.a`
-  color: ${({ theme }) => theme.secondaryDark};
-`
-const Bold = ({ children }) => <StyledB>{children}</StyledB>
+	color: ${({ theme }) => theme.secondaryDark};
+`;
+const Bold = ({ children }) => <StyledB>{children}</StyledB>;
 
-const StyledHyperlink = ({ children, href }) => (
-  <StyledA href={href}>{children}</StyledA>
-)
+const StyledHyperlink = ({ children, href }) => <StyledA href={href}>{children}</StyledA>;
 
 const options = {
-  renderNode: {
-    [BLOCKS.EMBEDDED_ASSET]: node => (
-      <ImageWrapper>
-        <ContentfulImage contentfulId={node.data.target.sys.id} />
-      </ImageWrapper>
-    ),
-    [INLINES.HYPERLINK]: node => (
-      <StyledHyperlink href={node.data.uri}>
-        {node.content[0].value}
-      </StyledHyperlink>
-    ),
-  },
-  renderMark: {
-    [MARKS.BOLD]: text => <Bold>{text}</Bold>,
-  },
-}
+	renderNode: {
+		[BLOCKS.EMBEDDED_ASSET]: node => (
+			<ImageWrapper>
+				<ContentfulImage contentfulId={node.data.target.sys.id} />
+			</ImageWrapper>
+		),
+		[INLINES.HYPERLINK]: node => <StyledHyperlink href={node.data.uri}>{node.content[0].value}</StyledHyperlink>
+	},
+	renderMark: {
+		[MARKS.BOLD]: text => <Bold>{text}</Bold>
+	}
+};
 
 const RichTextContenful = ({ richText }) => {
-  return <>{documentToReactComponents(richText, options)}</>
-}
+	return <>{documentToReactComponents(richText, options)}</>;
+};
 
-export default RichTextContenful
+export default RichTextContenful;
