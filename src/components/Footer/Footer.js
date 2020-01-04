8f93049ce6bc1e2b9ae3e2ed64b62e2c3fbe9304
mariusjagminas <mariusjagminas.it@gmail.com>
2020-01-04 20:50:58 +0100

    Change hardcoded date in footer to current

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,7 @@
 import React from "react"
 import styled from "styled-components"
 import Menu from "../Menu/Menu"
+import { format } from "date-fns"
 
 const Wrapper = styled.footer`
   width: 100%;
@@ -20,11 +21,23 @@ const Copyright = styled.p`
   box-shadow: 0px 4px 8px 0px rgba(255, 255, 255, 0.59);
 `
 
-const Footer = () => (
-  <Wrapper>
-    <Menu infooter="true" />
-    <Copyright> &copy; 2019 Pawel Hładki</Copyright>
-  </Wrapper>
-)
+class Footer extends React.Component {
+  state = {
+    year: "2019",
+  }
+
+  componentDidMount() {
+    this.setState({ year: format(new Date(), "yyyy") })
+  }
+
+  render() {
+    return (
+      <Wrapper>
+        <Menu infooter="true" />
+        <Copyright>&copy;{`${this.state.year} Pawel Hładki`} </Copyright>
+      </Wrapper>
+    )
+  }
+}
 
 export default Footer
