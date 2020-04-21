af567a6df76386a8c810d12d358e8dd5f1db5d3d
mariusjagminas <mariusjagminas.it@gmail.com>
2020-04-21 14:47:11 +0200

    Change text alignment to justify in ArticleExerpt

--- src/components/ArticleExcerpt/ArticleExcerpt.js
+++ src/components/ArticleExcerpt/ArticleExcerpt.js
@@ -5,6 +5,7 @@ import styled from "styled-components"
 
 const StyledQuote = styled.blockquote`
   margin: 0;
+  text-align: justify;
 `
 const ArticleExcerpt = ({ content, length, ...props }) => {
   return (
